48ae445f2322e1768c90c63dc4b485f2ab7783ca
Georgi Nikolov <project_it@abv.bg>
2016-10-12 13:15:17 +0300

    google sign in added

--- AvtovozBG/app/src/main/java/com/example/xcomputers/avtovozbg/MainActivity.java
+++ AvtovozBG/app/src/main/java/com/example/xcomputers/avtovozbg/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.xcomputers.avtovozbg;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks{
+
+    private static final int RC_SIGN_IN = 9001;
+    private TextView welcomeTV;
+    protected GoogleApiClient mGoogleApiClient;
+    private ProgressDialog mProgressDialog;
+    private SignInButton signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +40,154 @@ public class MainActivity extends AppCompatActivity {
         FirebaseMessaging.getInstance().subscribeToTopic("myTestTopic");
         String token = FirebaseInstanceId.getInstance().getToken();
         Log.e("TAG", token);
+
+        welcomeTV = (TextView) findViewById(R.id.welcomeTV);
+
+
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                    .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+
+
+        signInButton = (SignInButton) findViewById(R.id.google_login_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+            String displayName = result.getSignInAccount().getDisplayName();
+            String eMail = result.getSignInAccount().getEmail();
+            Log.e("displayName", displayName);
+            Log.e("eMail", eMail);
+            Log.e("tag",result.getStatus().getStatusCode() + "");
+            changeScreen();
+            finish();
+        }
+
+    }
+
+
+    private void handleSignInResult(GoogleSignInResult result) {
+
+        Log.e("TAG", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            String displayName = result.getSignInAccount().getDisplayName();
+            String eMail = result.getSignInAccount().getEmail();
+            Log.e("displayName", displayName);
+            Log.e("eMail", eMail);
+            Intent intent = new Intent(MainActivity.this, CarsActivity.class);
+            startActivity(intent);
+            finish();
+            hideProgressDialog();
+        } else {
+
+        }
+    }
+
+
+    @Override
+    public void onStart() {
+
+        mGoogleApiClient.connect();
+        showProgressDialog();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+
+            GoogleSignInResult result = opr.get();
+
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+
+        }
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.e("TAG", "onConnected called");
+        //changeScreen(true);
+    }
+    public void changeScreen(){
+        Intent intent = new Intent(MainActivity.this, CarsActivity.class);
+        startActivity(intent);
+        hideProgressDialog();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 }
